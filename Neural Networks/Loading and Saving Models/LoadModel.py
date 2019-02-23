a880897857b13ebf0165013b61ec36b25f361667
B0kCh01 <44041512+B0kCh01@users.noreply.github.com>
2019-02-23 11:16:04 -0800

    Update LoadModel.py

--- Neural Networks/Loading and Saving Models/LoadModel.py
+++ Neural Networks/Loading and Saving Models/LoadModel.py
@@ -1,35 +1,37 @@
+# Importing Libraries
+from keras.models import load_model
+import matplotlib.pyplot as plt
+from time import sleep
 from tensorflow import keras
 import numpy as np
 import matplotlib
-#fixes a bug
+# Fixes a bug with matplotlib
 matplotlib.use('TkAgg')
-from time import sleep
-import matplotlib.pyplot as plt
-from keras.models import load_model
 
-
+# Getting dataset
 fashionDataSet = keras.datasets.fashion_mnist
-(train_images, train_labels), (test_images, test_labels) = fashionDataSet.load_data()
+(train_images, train_labels), (test_images,
+                               test_labels) = fashionDataSet.load_data()
 
-#class names (y) 
-classNames = ['t-shirt', 'pants', 'hoodie', 'dress', 'coat', 
+# class names (y) for future reference
+classNames = ['t-shirt', 'pants', 'hoodie', 'dress', 'coat',
               'sandal', 'shirt', 'sneaker', 'bag', 'ankle boot']
 
-#asking user what to test
+# Asking user what to test
 labrat = int(input("Test number: "))
 
-#what it says below:
+# Graphing the image
 print("Seting up the first visual figure")
 plt.figure()
 plt.imshow(test_images[labrat])
 plt.colorbar()
 plt.grid(False)
 
-#Making the classes in a visual format
+# Making a visual
 print("Seting up the second visual figure")
-plt.figure(figsize=(10,10))
+plt.figure(figsize=(10, 10))
 for i in range(25):
-    plt.subplot(5,5,i+1)
+    plt.subplot(5, 5, i + 1)
     plt.xticks([])
     plt.yticks([])
     plt.grid(False)
@@ -39,19 +41,19 @@ for i in range(25):
 print("Would you like to see the visuals?")
 answer = input("y/n: ")
 if (answer == "y"):
-	plt.show()
+    plt.show()
 
-#convert pixels color to values between 1-0 (orginial 0-225)
+# convert pixels color to values between 1-0 (orginial 0-225)
 test_images = test_images / 255.00
 
-#Loading model and storing it in a variable
+# Loading model and saving as "nuerelNetModel"
 nuerelNetModel = load_model('my_model.h5')
 
+# Using the Network (Testing it)
 testLoss, testAccuracy = nuerelNetModel.evaluate(test_images, test_labels)
 print("Accuracy during testing: ", testAccuracy)
 predictions = nuerelNetModel.predict(test_images)
 print(predictions[labrat], "\n")
-print()
 typeOfClothing = (np.argmax(predictions[labrat]))
 print("The Neurel Net says its a form of " + classNames[typeOfClothing] + ".")
 print("It's a " + classNames[test_labels[labrat]])
