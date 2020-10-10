8fc442cd3a139ecd6e6d0a3f352e7bfc8470365e
b0kch01 <nath49ers@gmail.com>
2020-10-10 09:55:33 -0700

    🎯 Improve Accuracy

--- Neural Networks/Tensorflow/Heart Disease/Heart_Disease_Classifier.py
+++ Neural Networks/Tensorflow/Heart Disease/Heart_Disease_Classifier.py
@@ -17,7 +17,7 @@ X = dataset.iloc[:, :-1].values # X is all collumns except the last one
 y = dataset.iloc[:, -1].values # y is the last collumn
 
 # Here is the basic syntax for splitting the x and y into test and training sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
 # Make a new "object" for standardscaling
 sc = StandardScaler()
 # Tune sc and then changing all the values
@@ -25,16 +25,17 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Printing the amount of testing and training values
-print("Sizes:\nTrain: {}\nTest: {}".format(len(y_train), len(y_test)))
+print("Train: {}\nTest: {}".format(len(y_train), len(y_test)))
 
 # Function to design the model (using a function isn't neccessary but can look cleaner)
 def create_model():
 	# You know the usual
 	nn = models.Sequential()
-	nn.add(layers.Dense(200, activation="relu"))
-	nn.add(layers.Dropout(0.2)) # This is to prevent overfitting
-	nn.add(layers.Dense(75, activation="relu"))
-	nn.add(layers.Dropout(0.2)) # So is this one
+	nn.add(layers.Dense(32, activation="relu"))
+	nn.add(layers.Dropout(0.5)) # This is to prevent overfitting
+	nn.add(layers.Dense(16, activation="relu"))
+	nn.add(layers.Dropout(0.5)) # So is this one
+	nn.add(layers.Dense(8, activation="relu"))
 	nn.add(layers.Dense(1, activation="sigmoid")) # Sigmoid becuase the activation returns a number 0-1
 	return nn
 
@@ -45,8 +46,23 @@ model.compile(
 	optimizer="adam", # Adam is just good
 	loss="binary_crossentropy", # binary because its only 1 and 0
 	metrics=["accuracy"] # accuracy works
-	)
+)
 
 # The model trains relitively fast so I made it quiet
-model.fit(X_train, y_train, epochs=120, verbose=0) # Training the model. 120 is a good number of epochs
+history = model.fit(X_train, y_train, epochs=200, verbose=0) # Training the model. 50 is a good numbe1r of epochs
 model.evaluate(X_test, y_test, verbose=0) # Testing it to test how it does in the real world
+
+# Plot the data
+import matplotlib.pyplot as plt
+
+plt.plot(history.history['loss'])
+plt.title('Training Loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+
+plt.plot(history.history['accuracy'])
+plt.title('Training Accuracy')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['loss', 'acc'], loc='upper left')
+plt.show()
